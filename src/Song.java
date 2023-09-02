102c12d2c731c501de77444bcb8d87f154cfb1b1
ZoeHaynes <zoeh2025@student.cis.edu.hk>
2023-09-02 15:33:41 +0800

    Completed

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,46 @@
-public class Song {
+import java.util.ArrayList;
 
-    private String
+public class Song {
+    //properties
+    private String title;
+
+    private String artist;
+
+    private ArrayList<String> totalListeners;
+
+    //constructors
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        this.totalListeners = new ArrayList<>();
+    }
+
+    //methods
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> al){
+        int newListeners = 0;
+        for(int i = 0; i<al.size(); i++){
+            al.set(i,al.get(i).toLowerCase());
+
+        }
+        for(int i = 0; i<al.size(); i++){
+            if(!totalListeners.contains(al.get(i))){
+                newListeners++;
+                totalListeners.add(al.get(i));
+            }
+
+        }
+
+        return newListeners;
+
+
+    }
+
 }
